985f80097b842eb0a4f9f04d23e5d71886e702ef
MilesCranmerBot <miles.cranmer.bot@gmail.com>
2026-02-13 10:01:03 +0000

    CI: loop 100x; clarify turbo forced on

--- run.jl
+++ run.jl
@@ -6,5 +6,5 @@ include("test/integration/ext/loopvectorization/test_mixed_utils.jl")
 
 # Run the specific configuration used in the upstream integration test item:
 # "Search with multiprocessing & default hyperparameters & Float64 type & turbo"
-# (the test code disables `turbo` on Linux < 1.11).
+# (this MWE forces `turbo = true` for the i==5 case, even on Linux + Julia 1.10).
 test_mixed(5, false, false, :multiprocessing)
